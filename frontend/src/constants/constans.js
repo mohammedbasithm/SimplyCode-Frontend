18683cd44b5915544e291944000482b8f8900381
mohammed basith <mohammedbasith8111@gmail.com>
2024-01-13 09:25:59 +0530

    change the base url

--- frontend/src/constants/constans.js
+++ frontend/src/constants/constans.js
@@ -1,4 +1,4 @@
-export const BASE_URL="http://127.0.0.1:8000/api"
-// export const BASE_URL="https://symplycode.basith.shop/api"
-export const Image_URL='http://127.0.0.1:8000'
-// export const Image_URL='https://symplycode.basith.shop'
+// export const BASE_URL="http://127.0.0.1:8000/api"
+export const BASE_URL="https://symplycode.basith.shop/api"
+// export const Image_URL='http://127.0.0.1:8000'
+export const Image_URL='https://symplycode.basith.shop'
